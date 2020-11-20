5056a0b6ab30a8582aa306152218337a49266f79
Mario Aksiyote <maksiyote@gmail.com>
2020-11-20 13:57:21 -0500

    added div instead of fragment to global layout. Might fix styling problem on deploy

--- src/layouts/global.js
+++ src/layouts/global.js
@@ -2,14 +2,14 @@ import React from "react"
 import { Navbar, Footer, NavBlob } from "Components";
 
 const GlobalLayout = ({ element, props }) => (
-  <>
+  <div>
     {/* <SEO />  */}
     {/* <GrainyBackground/> */}
     <NavBlob pathname={props.location.pathname}/>
     <Navbar/>
     {element}
     <Footer/>
-  </>
+  </div>
 )
 
 
