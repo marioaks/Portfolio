3ee27eb4248e4c90f92664d8bc268c6e88ac366f
Mario Aksiyote <maksiyote@gmail.com>
2020-11-20 02:08:19 -0500

    added global document check to prevent build from failing

--- src/utils/index.js
+++ src/utils/index.js
@@ -22,32 +22,34 @@ export const mapRange = (value, in_min, in_max, out_min, out_max) => {
 };
 
 export const getNumericStyleProp = (el, styleProp) => {
-  var value, defaultView = (el.ownerDocument || document).defaultView;
-  // W3C standard way:
-  if (defaultView && defaultView.getComputedStyle) {
-    // sanitize property name to css notation
-    // (hypen separated words eg. font-Size)
-    styleProp = styleProp.replace(/([A-Z])/g, "-$1").toLowerCase();
-    return parseFloat(defaultView.getComputedStyle(el, null).getPropertyValue(styleProp));
-  } else if (el.currentStyle) { // IE
-    // sanitize property name to camelCase
-    styleProp = styleProp.replace(/(\w)/g, function(str, letter) {
-      return letter.toUpperCase();
-    });
-    value = el.currentStyle[styleProp];
-    // convert other units to pixels on IE
-    if (/^\d+(em|pt|%|ex)?$/i.test(value)) { 
-      return (function(value) {
-        var oldLeft = el.style.left, oldRsLeft = el.runtimeStyle.left;
-        el.runtimeStyle.left = el.currentStyle.left;
-        el.style.left = value || 0;
-        value = el.style.pixelLeft + 'px';
-        el.style.left = oldLeft;
-        el.runtimeStyle.left = oldRsLeft;
-        return value;
-      })(value);
+  if (typeof document !== `undefined`) {
+    var value, defaultView = (el.ownerDocument || document).defaultView;
+    // W3C standard way:
+    if (defaultView && defaultView.getComputedStyle) {
+      // sanitize property name to css notation
+      // (hypen separated words eg. font-Size)
+      styleProp = styleProp.replace(/([A-Z])/g, "-$1").toLowerCase();
+      return parseFloat(defaultView.getComputedStyle(el, null).getPropertyValue(styleProp));
+    } else if (el.currentStyle) { // IE
+      // sanitize property name to camelCase
+      styleProp = styleProp.replace(/(\w)/g, function(str, letter) {
+        return letter.toUpperCase();
+      });
+      value = el.currentStyle[styleProp];
+      // convert other units to pixels on IE
+      if (/^\d+(em|pt|%|ex)?$/i.test(value)) { 
+        return (function(value) {
+          var oldLeft = el.style.left, oldRsLeft = el.runtimeStyle.left;
+          el.runtimeStyle.left = el.currentStyle.left;
+          el.style.left = value || 0;
+          value = el.style.pixelLeft + 'px';
+          el.style.left = oldLeft;
+          el.runtimeStyle.left = oldRsLeft;
+          return value;
+        })(value);
+      }
+      return parseFloat(value);
     }
-    return parseFloat(value);
   }
 }
 
